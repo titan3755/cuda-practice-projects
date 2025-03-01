e7612571131a2b716646887d7bebf464ec5560c8
Titan <68808479+titan3755@users.noreply.github.com>
2025-03-01 20:35:36 +0600

    added basic cuda math op programs

---
+++ addition.cu
@@ -0,0 +1,42 @@
+﻿#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
+#include <stdio.h>
+
+__global__ void addition(int* a, int* b, int* c) {
+    int i = threadIdx.x + blockDim.x * blockIdx.x;
+    c[i] = a[i] + b[i];
+}
+
+__managed__ int a[256], b[256], c[256];
+
+// name of main function changed to enable testing of multiple files
+
+int one_main() {
+    for (int i = 0; i < 256; i++) {
+        a[i] = i;
+        b[i] = i * (i - (i - 20));
+    }
+
+    int* d_a, * d_b, * d_c;
+    cudaMalloc(&d_a, 256 * sizeof(int));
+    cudaMalloc(&d_b, 256 * sizeof(int));
+    cudaMalloc(&d_c, 256 * sizeof(int));
+
+    cudaMemcpy(d_a, a, 256 * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, 256 * sizeof(int), cudaMemcpyHostToDevice);
+
+    addition <<<1, 256 >>> (d_a, d_b, d_c);
+
+    cudaMemcpy(c, d_c, 256 * sizeof(int), cudaMemcpyDeviceToHost);
+
+    for (int i = 0; i < 256; i++) {
+        printf("%d + %d = %d\n", a[i], b[i], c[i]);
+    }
+
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+
+    return 0;
+}
---
+++ multiplication.cu
@@ -0,0 +1,40 @@
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
+#include <stdio.h>
+
+__global__ void multiplication(int* a, int* b, int* c) {
+    int i = threadIdx.x + blockDim.x * blockIdx.x;
+    c[i] = a[i] * b[i];
+}
+
+__managed__ int a[256], b[256], c[256];
+
+int two_main() {
+    for (int i = 0; i < 256; i++) {
+        a[i] = i;
+        b[i] = i * (i - (i - 5));
+    }
+
+    int* d_a, * d_b, * d_c;
+    cudaMalloc(&d_a, 256 * sizeof(int));
+    cudaMalloc(&d_b, 256 * sizeof(int));
+    cudaMalloc(&d_c, 256 * sizeof(int));
+
+    cudaMemcpy(d_a, a, 256 * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, 256 * sizeof(int), cudaMemcpyHostToDevice);
+
+    multiplication << <1, 256 >> > (d_a, d_b, d_c);
+
+    cudaMemcpy(c, d_c, 256 * sizeof(int), cudaMemcpyDeviceToHost);
+
+    for (int i = 0; i < 256; i++) {
+        printf("%d * %d = %d\n", a[i], b[i], c[i]);
+    }
+
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+
+    return 0;
+}
